6edd29b0ccd912a8f2b07c9765ee43cdbca56108
ExStas_Poh <monax071595@gmail.com>
2023-03-01 20:46:33 +0300

    Constants

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://i.pinimg.com/originals/b9/05/3d/b9053d873e9f69058997913e0fffca2e.png">
+            </img>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://proprikol.ru/wp-content/uploads/2020/05/gorizont-krasivye-kartinki-10.jpg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    )
+}
