4b187570293d2acaf680f1ba72ba494d18ea59b7
ExStas_Poh <monax071595@gmail.com>
2023-03-01 20:29:52 +0300

    Create soc-set

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
-import {Technologies} from "./Technologies";
+
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://i.pinimg.com/originals/b9/05/3d/b9053d873e9f69058997913e0fffca2e.png">
+                </img>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://proprikol.ru/wp-content/uploads/2020/05/gorizont-krasivye-kartinki-10.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New Post
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Header = () => {
-    return (
-        <div>
-            <h1>Hello, samurai! Let's go!</h1>
-        </div>
-    )
-}
